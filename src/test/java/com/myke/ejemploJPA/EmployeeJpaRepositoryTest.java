6b25f17fad5a719605f41e04fc52e3f3e5821c1f
MykeStv <mykestv22@gmail.com>
2022-02-18 16:49:32 -0500

    Test realizado exitosamente con h2

--- src/test/java/com/myke/ejemploJPA/EmployeeJpaRepositoryTest.java
+++ src/test/java/com/myke/ejemploJPA/EmployeeJpaRepositoryTest.java
@@ -1,9 +1,33 @@
 package com.myke.ejemploJPA;
 
+import com.myke.ejemploJPA.model.Employee;
+import com.myke.ejemploJPA.repository.IEmployeeJpaRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class EmployeeJpaRepositoryTest {
+    @Autowired
+    private IEmployeeJpaRepository repo;
+
+    @Test
+    public void saveEmployee() {
+        Employee john = new Employee("John", "Smith", "empl123");
+        Employee claire = new Employee("Claire", "Mitz", "empl124");
+
+        repo.save(john);
+        repo.save(claire);
+
+        repo.flush();
+
+        assertEquals(2, repo.findAll().size());
+    }
+
 }
